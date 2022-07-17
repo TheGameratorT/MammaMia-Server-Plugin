239b35876cdbaedd766a18a96ed317b2c7bc6722
Tiago Silva <tiagolsilva14@gmail.com>
2022-07-17 10:34:35 +0100

    Update to 1.19

--- src/main/java/com/thegameratort/mammamia/MammaMiaCommand.java
+++ src/main/java/com/thegameratort/mammamia/MammaMiaCommand.java
@@ -1,5 +1,6 @@
 package com.thegameratort.mammamia;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -32,6 +33,27 @@ public class MammaMiaCommand implements CommandExecutor, TabExecutor
                 if (sender instanceof Player)
                     MammaMiaMenu.open(null, (Player) sender);
             }
+            else if (args[0].equals("respawn"))
+            {
+                if (args.length < 2)
+                {
+                    sender.sendMessage(ChatColor.RED + "You need to specify a player to respawn.");
+                }
+                else
+                {
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if (target != null)
+                    {
+                        if (!target.isDead())
+                            target.setHealth(0);
+                        target.spigot().respawn();
+                    }
+                    else
+                    {
+                        sender.sendMessage(ChatColor.RED + "Could not find that player.");
+                    }
+                }
+            }
         }
         return true;
     }
@@ -40,7 +62,19 @@ public class MammaMiaCommand implements CommandExecutor, TabExecutor
     {
         List<String> list = new ArrayList<>();
         if (args.length == 1)
+        {
             list.add("menu");
+            list.add("respawn");
+        }
+        else if (args.length == 2)
+        {
+            if (args[0].equals("respawn"))
+            {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    list.add(player.getName());
+                }
+            }
+        }
         return list;
     }
 }
